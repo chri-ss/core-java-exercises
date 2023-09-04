a76801524ebabbba10377ba16e771faacf3d20a1
chri-ss <christorper@gmail.com>
2023-09-03 18:45:17 -0700

    ex05 precision loss on double to int cast

---
+++ ch01/ex05/InvalidCast.java
@@ -0,0 +1,8 @@
+public class InvalidCast {
+    public static void main(String[] args) {
+        double veryLarge = 10000000000000000000.0;
+
+
+        System.out.println((int) veryLarge);
+    }
+}
